aded55012450f544aa3ce02c314ebb075fbee153
andrew_kandzuba2 <andrey.kandzuba@gmail.com>
2023-06-01 16:16:42 -0500

    complete VPC without LB project configuration

---
+++ vpc_no_lb/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Name        = "Provider Tag",
+      Purpose     = "CDA 2023"
+      Stack       = "test"
+    }
+  }
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+}
+
+module "ec2" {
+  source = "../modules/ec2"
+
+  cda_public_subnets = module.vpc.cda_public_subnets
+  cda_security_groups = [aws_security_group.ec2_sg_http.id, aws_security_group.ec2_sg_ssh.id]
+  cda_availability_zones = [module.vpc.cda_availability_zones]
+  boot-script = pathexpand("./ec2-user-data.sh")
+}
+
+resource "aws_security_group" "ec2_sg_http" {
+  vpc_id = module.vpc.cda_vpc_id
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [module.vpc.all_subnets_cidr]
+  }
+
+  // Only reachable via ALB instance
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [module.vpc.all_subnets_cidr]
+  }
+
+  tags = {
+    Name = "cda-ec2-sg-http"
+  }
+}
+
+resource "aws_security_group" "ec2_sg_ssh" {
+  vpc_id = module.vpc.cda_vpc_id
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [module.vpc.all_subnets_cidr]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [module.vpc.all_subnets_cidr]
+  }
+
+  tags = {
+    Name = "cda-ec2-sg-ssh"
+  }
+}
